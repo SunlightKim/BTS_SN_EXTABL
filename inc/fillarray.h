868d4806e82173530e7054ae082b5d93889146a1
SunlightKim <104458119+SunlightKim@users.noreply.github.com>
2022-10-28 14:35:22 +0200

    :pencil2: [FIX] Fix wrong project name in header definition

--- inc/fillarray.h
+++ inc/fillarray.h
@@ -2,8 +2,8 @@
 // Created by enzbonolis@uimm-sn.fc on 28/10/22.
 //
 
-#ifndef UNTITLED_FILLARRAY_H
-#define UNTITLED_FILLARRAY_H
+#ifndef BTS_SN_EXTABL_FILLARRAY_H
+#define BTS_SN_EXTABL_FILLARRAY_H
 
 #include "const.h"
 
@@ -13,4 +13,4 @@ void fillBorder(int array[SIZE][SIZE]);
 void fillCells(int array[SIZE][SIZE]);
 void fillDiag(int array[SIZE][SIZE]);
 
-#endif //UNTITLED_FILLARRAY_H
+#endif //BTS_SN_EXTABL_FILLARRAY_H
